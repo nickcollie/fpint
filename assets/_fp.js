ca412310951ba537cac5e5a0cfdf810a14429789
Niccol <mail@nickcollie.co.uk>
2022-12-01 16:21:36 +0000

    Dec 1

---
+++ assets/_fp.js
@@ -0,0 +1,8 @@
+function openNavLeft() {
+document.getElementById("shopify-section-off-canvas-left").style.width = "300px";
+}
+
+function closeNavLeft() {
+document.getElementById("shopify-section-off-canvas-left").style.width = "0";
+}
+
