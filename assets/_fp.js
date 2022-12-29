70a1ceec6dbe5e3a3d9010757de54bbd923b0ae9
Niccol <mail@nickcollie.co.uk>
2022-12-29 15:35:38 +0000

    Dec 29

--- assets/_fp.js
+++ assets/_fp.js
@@ -1,5 +1,6 @@
 function openNavLeft() {
-document.getElementById("shopify-section-off-canvas-left").style.width = "300px";
+  console.log('open');
+  document.getElementById("shopify-section-off-canvas-left").style.width = "300px";
 }
 
 function closeNavLeft() {
@@ -7,19 +8,6 @@ document.getElementById("shopify-section-off-canvas-left").style.width = "0";
 }
 
 window.addEventListener('scroll', function () {
-  if(window.pageYOffset > 50) {
-  	document.getElementById('fp_hamburger_top').style.visibility = "hidden";
-  	document.getElementById('fp_hamburger').style.visibility = "visible";
-  	document.getElementById('fp_icons_top').style.visibility = "hidden";
-  	document.getElementById('fp_icons').style.visibility = "visible";
-    
-  } else {
-  	document.getElementById('fp_hamburger_top').style.visibility = "visible";
-  	document.getElementById('fp_hamburger').style.visibility = "hidden";
-  	document.getElementById('fp_icons_top').style.visibility = "visible";
-  	document.getElementById('fp_icons').style.visibility = "hidden";
-    
-  }
 
   if(window.pageYOffset > 200) { 
     $('#fp_header_scroll').css("background","rgba(0,0,0,0.9)");
