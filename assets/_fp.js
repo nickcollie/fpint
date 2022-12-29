74f8b1efa965db44b6aa6980aa9cc2e58e636b1e
Niccol <mail@nickcollie.co.uk>
2022-12-29 10:37:52 +0000

    Comics Fonts 2

--- assets/_fp.js
+++ assets/_fp.js
@@ -7,17 +7,23 @@ document.getElementById("shopify-section-off-canvas-left").style.width = "0";
 }
 
 window.addEventListener('scroll', function () {
-  if(window.pageYOffset > 100) {
+  if(window.pageYOffset > 50) {
   	document.getElementById('fp_hamburger_top').style.visibility = "hidden";
   	document.getElementById('fp_hamburger').style.visibility = "visible";
   	document.getElementById('fp_icons_top').style.visibility = "hidden";
   	document.getElementById('fp_icons').style.visibility = "visible";
-    $('#fp_header_scroll').css("background","rgba(0,0,0,0.9)");
+    
   } else {
   	document.getElementById('fp_hamburger_top').style.visibility = "visible";
   	document.getElementById('fp_hamburger').style.visibility = "hidden";
   	document.getElementById('fp_icons_top').style.visibility = "visible";
   	document.getElementById('fp_icons').style.visibility = "hidden";
+    
+  }
+
+  if(window.pageYOffset > 200) { 
+    $('#fp_header_scroll').css("background","rgba(0,0,0,0.9)");
+  } else {
     $('#fp_header_scroll').css("background","transparent");
   }
 });
